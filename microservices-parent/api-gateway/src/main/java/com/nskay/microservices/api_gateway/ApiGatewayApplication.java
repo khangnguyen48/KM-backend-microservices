0ece1b99a19e5361df7705b19f2670380ceaaac3
khangnguyen48 <21522193@gm.uit.edu.vn>
2024-07-26 15:48:11 +0700

    merge all services into one parent project

---
+++ microservices-parent/api-gateway/src/main/java/com/nskay/microservices/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nskay.microservices.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ microservices-parent/src/test/java/com/nskay/microservices/microservicesparent/MicroservicesParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nskay.microservices.microservicesparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
