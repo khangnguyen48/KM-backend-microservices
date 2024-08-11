0a118fbddcaddc1319c134b97fe8d8a7c46b476d
khangnguyen48 <21522193@gm.uit.edu.vn>
2024-08-11 11:02:30 +0700

    feat: Use REST Client to communicate between services instead of OpenFeign

---
+++ order-service/src/main/java/com/nskay/microservices/order/config/RestClientConfig.java
@@ -0,0 +1,30 @@
+package com.nskay.microservices.order.config;
+
+import com.nskay.microservices.order.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+
+import java.util.Map;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;
+
+    @Bean
+    public InventoryClient inventoryClient(){
+        RestClient restClient = RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+}
